ed48547b43a7386552cda3a32cce6affa97d3c0c
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-25 12:49:06 +0300

    feat: add interaction with console

--- src/main/java/ru/spbstu/telematics/java/SystemThread.java
+++ src/main/java/ru/spbstu/telematics/java/SystemThread.java
@@ -3,11 +3,22 @@ package ru.spbstu.telematics.java;
 public class SystemThread extends Thread {
     private final BinarySemaphore semaphore;
 
-    public SystemProcessThread(BinarySemaphore semaphore) {
+    public SystemThread(BinarySemaphore semaphore) {
         this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
+        try {
+            semaphore.acquire();
+            // Логика работы системного процесса с консолью
+            System.out.println("Системный процесс " + this.getName() + " начал взаимодействие с консолью.");
+            Thread.sleep(1000); // Имитация работы с консолью
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+            System.out.println("Системный процесс " + this.getName() + " закончил взаимодействие с консолью.");
+        }
     }
 }
