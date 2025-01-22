df0739a8008034ea6bc83817b8ff1c61fb767bd4
StanleyStanMarsh <iastafev56@gmail.com>
2025-01-22 17:04:44 +0300

    refactor: add constructors

--- src/main/java/ru/spbstu/telematics/java/BinarySemaphore.java
+++ src/main/java/ru/spbstu/telematics/java/BinarySemaphore.java
@@ -4,11 +4,19 @@ package ru.spbstu.telematics.java;
 public class BinarySemaphore {
     private final volatile boolean isLocked;
 
+    public BinarySemaphore() {
+        this.isLocked = false;
+    }
+
+    public BinarySemaphore(boolean initialState) {
+        this.isLocked = initialState;
+    }
+
     public void acquire() throws InterruptedException {
         
     }
 
     public void release() {
-        
+
     }
 }
