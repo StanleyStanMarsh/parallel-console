863df67d169fe72fe311a088d4488b0831c5f4d2
StanleyStanMarsh <iastafev56@gmail.com>
2025-01-22 17:02:17 +0300

    refactor: add empty methods acquire and release

--- src/main/java/ru/spbstu/telematics/java/BinarySemaphore.java
+++ src/main/java/ru/spbstu/telematics/java/BinarySemaphore.java
@@ -1,15 +1,14 @@
 package ru.spbstu.telematics.java;
-import java.util.concurrent.Semaphore;
 
 
 public class BinarySemaphore {
-    private final Semaphore semaphore = new Semaphore(1);
+    private final volatile boolean isLocked;
 
     public void acquire() throws InterruptedException {
-        semaphore.acquire();
+        
     }
 
     public void release() {
-        semaphore.release();
+        
     }
 }
